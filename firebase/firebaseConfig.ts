55e51e98a1180380690c53f420c36750320d320f
manu-sreesanth <healthflayer@gmail.com>
2025-09-10 16:18:44 +0530

    Remove unused groqClient file

--- firebase/firebaseConfig.ts
+++ firebase/firebaseConfig.ts
@@ -3,19 +3,18 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAnx1xikqP7fGVyNuuu2MHwYLsXDIUrJqw",
-  authDomain: "tomo2-3625a.firebaseapp.com",
-  projectId: "tomo2-3625a",
-  storageBucket: "tomo2-3625a.firebasestorage.app",
-  messagingSenderId: "1096219789551",
-  appId: "1:1096219789551:web:8d7fdd44e5df44b1b7b31a"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-
-// Enable ignoreUndefinedProperties to avoid Firestore errors
 const db = getFirestore(app);
 
 export { auth, db };
+
 
